7c8e56c62faed564ed47a9c38230f3e12c7c3c27
royitaqi <royitaqi@users.noreply.github.com>
2022-06-22 22:05:02 -0700

    Bump version to 0.1.2.0 for the added TK dream death detection and active loss generation.

--- GodhomeWinLossTracker/GodhomeWinLossTracker.cs
+++ GodhomeWinLossTracker/GodhomeWinLossTracker.cs
@@ -24,7 +24,7 @@ namespace GodhomeWinLossTracker
         ///
 
         // <breaking change>.<non-breaking big feature/fix>.<non-breaking small feature/fix>.<patch>
-        public override string GetVersion() => "0.1.1.1";
+        public override string GetVersion() => "0.1.2.0";
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
 #if DEBUG
