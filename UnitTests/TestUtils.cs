8f1819f1394311b1625152b3236bba738f132296
royitaqi <royitaqi@users.noreply.github.com>
2022-06-24 20:28:18 -0700

    Add/fix test cases in TestWinLossGenerator

--- UnitTests/TestUtils.cs
+++ UnitTests/TestUtils.cs
@@ -21,14 +21,62 @@ namespace UnitTests
             public IEnumerable<IHandler> Handlers { get; set; }
             public IEnumerable<IMessage> InputMessages { get; set; }
             public IEnumerable<IMessage> ExpectedMessages { get; set; }
+            public G.AssertionFailedException ExpectedException { get; set; }
+        }
+
+        public static void TestMessageBus(IEnumerable<MessageBusTestCase> testCases)
+        {
+            foreach (var testCase in testCases)
+            {
+                TestMessageBus(testCase);
+            }
         }
 
         public static void TestMessageBus(MessageBusTestCase testCase)
         {
-            TestMessageBus(testCase.Name, testCase.Handlers, testCase.InputMessages, testCase.ExpectedMessages);
+            // Only one of the these can be nonnull.
+            Assert.IsTrue((testCase.ExpectedMessages == null) != (testCase.ExpectedException == null));
+
+            if (testCase.ExpectedMessages != null)
+            {
+                TestMessageBus(testCase.Name, testCase.Handlers, testCase.InputMessages, testCase.ExpectedMessages);
+            }
+            else
+            {
+                TestMessageBus(testCase.Name, testCase.Handlers, testCase.InputMessages, testCase.ExpectedException);
+            }
         }
 
         public static void TestMessageBus(string testName, IEnumerable<IHandler> handlers, IEnumerable<IMessage> inputMessages, IEnumerable<IMessage> expectedMessages)
+        {
+            IEnumerable<IMessage> outputMessages = RunMessageBus(testName, handlers, inputMessages);
+
+            // Verify output messages
+            Assert.AreEqual(
+                TestUtils.ConvertMessagesToString(new[] { new BusEvent { Event = "initialized" } }.Concat(expectedMessages)),
+                TestUtils.ConvertMessagesToString(outputMessages),
+                testName
+            );
+        }
+
+        public static void TestMessageBus(string testName, IEnumerable<IHandler> handlers, IEnumerable<IMessage> inputMessages, G.AssertionFailedException expectedException)
+        {
+            try
+            {
+                RunMessageBus(testName, handlers, inputMessages);
+                throw new AssertFailedException($"Should throw AssertionFailedException(\"{expectedException.Message}\")");
+            }
+            catch (G.AssertionFailedException ex)
+            {
+                Assert.AreEqual(expectedException.Message, ex.Message, "AssertionFailedException thrown with unexpected message");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static IEnumerable<IMessage> RunMessageBus(string testName, IEnumerable<IHandler> handlers, IEnumerable<IMessage> inputMessages)
         {
             // Create objects
             Mock<G.IGodhomeWinLossTracker> mod = new();
@@ -41,20 +89,7 @@ namespace UnitTests
                 bus.Put(inputMessage);
             }
 
-            // Verify output messages
-            Assert.AreEqual(
-                TestUtils.ConvertMessagesToString(new[] { new BusEvent { Event = "initialized" } }.Concat(expectedMessages)),
-                TestUtils.ConvertMessagesToString(recorder.Messages),
-                testName
-            );
-        }
-
-        public static void TestMessageBus(IEnumerable<MessageBusTestCase> testCases)
-        {
-            foreach (var testCase in testCases)
-            {
-                TestMessageBus(testCase);
-            }
+            return recorder.Messages;
         }
     }
 }
