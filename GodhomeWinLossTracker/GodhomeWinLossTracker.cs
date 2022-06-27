e920cdc4c1e4269fcd5aff1c7ee09c46784f8862
royitaqi <royitaqi@users.noreply.github.com>
2022-06-26 17:33:22 -0700

    Bump version to 0.2.2.0 for this bug fix.

--- GodhomeWinLossTracker/GodhomeWinLossTracker.cs
+++ GodhomeWinLossTracker/GodhomeWinLossTracker.cs
@@ -25,7 +25,7 @@ namespace GodhomeWinLossTracker
         ///
 
         // <breaking change>.<non-breaking big feature/fix>.<non-breaking small feature/fix>.<patch>
-        public override string GetVersion() => "0.2.1.0";
+        public override string GetVersion() => "0.2.2.0";
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
 #if DEBUG
