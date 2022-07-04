b1dc40a227d936b33ffebc0dc1f6c1f2392ae6a3
royitaqi <royitaqi@users.noreply.github.com>
2022-07-03 21:49:32 -0700

    Remove unit tests just to be clear

--- UnitTests/Usings.cs
+++
@@ -1,9 +0,0 @@
-global using G = GodhomeWinLossTracker;
-global using GodhomeWinLossTracker.MessageBus.Handlers;
-global using GodhomeWinLossTracker.MessageBus.Messages;
-global using GodhomeWinLossTracker.MessageBus;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Moq;
-global using Newtonsoft.Json;
-global using System.Diagnostics;
-global using System.Text;
