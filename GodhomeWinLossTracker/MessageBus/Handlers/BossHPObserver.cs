95dcd473b067085a96000d157d9c6766c21d457e
royitaqi <royitaqi@users.noreply.github.com>
2022-07-09 17:45:13 -0700

    Clamp boss HP to 0~1.

--- GodhomeWinLossTracker/MessageBus/Handlers/BossHPObserver.cs
+++ GodhomeWinLossTracker/MessageBus/Handlers/BossHPObserver.cs
@@ -5,6 +5,48 @@ using GodhomeWinLossTracker.MessageBus.Messages;
 
 namespace GodhomeWinLossTracker.MessageBus.Handlers
 {
+    // Special cases:
+    // - The Collector:
+    //   - Fighting: 900/900
+    //   - Death anim: -50/-50
+    // - Oro & Mato:
+    //   - Starting:
+    //     - Mato = 1000/1000
+    //   - 1st phase:
+    //     - Oro = 500/500
+    //     - Mato = 1000/1000, invis
+    //   - Intermission:
+    //     - Oro = -50/500
+    //     - Mato = 1000/1000
+    //   - 2nd phase
+    //     - Oro = 600/500
+    //     - Mato = 1000/1000
+    //   - Death anim
+    //     - Oro = -50/500
+    //     - Mato = -1/1000
+    // - God Tamer:
+    //   - Lobster (God) = 750
+    //   - Lancer (Tamer) = 750, not critical
+    // - Sisters of Battle:
+    //   - 1st phase:
+    //     - Mantis Lord = 500 \
+    //     - Mantis Lord = 500 / these two are connected
+    //   - 2nd phase:
+    //     - Mantis Lord S1 = 750 \
+    //     - Mantis Lord S1 = 750 /
+    //     - Mantis Lord S2 = 750 \
+    //     - Mantis Lord S2 = 750 /
+    //     - Mantis Lord S3 = 750 \
+    //     - Mantis Lord S3 = 750 /
+    // - AbsRad
+    //   - 1st phase: 2280/2280
+    //   - 2nd phase: 1880/2280
+    //   - 3rd phase: 1430/2280
+    //   - 4th phase: 1130/2280
+    //   - 5th phase: 380/2280
+    //   (reset to 280 no matter the health)
+    //   - 6th phase: 280/2280
+
     internal class BossHPObserver : Handler
     {
         public void OnBossChange(TheMessageBus bus, Modding.ILogger logger, BossChange msg)
@@ -32,13 +74,13 @@ namespace GodhomeWinLossTracker.MessageBus.Handlers
         {
             if (_isInFight)
             {
-                bus.Put(new BossHP { MaxHP = _maxHP, HP = _healthManagers.Select(hm => GetHP(hm)).Sum() });
+                bus.Put(new BossHP { MaxHP = _maxHP, HP = Math.Min(_maxHP, _healthManagers.Select(hm => GetHP(hm)).Sum()) });
             }
         }
 
         private int GetHP(HealthManager hm)
         {
-            return hm.isDead ? 0 : Math.Max(hm.hp, 0);
+            return hm.isDead ? 0 : Math.Max(0, hm.hp);
         }
 
         private bool IsBoss(HealthManager hm)
